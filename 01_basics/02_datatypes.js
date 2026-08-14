5aaaaeee1ec7b120d42afdcd920f0d37c26db306
JPSOMRAN <141428557+JPSOMRAN@users.noreply.github.com>
2026-08-14 13:28:52 +0000

    container config done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,7 +1,8 @@
 "use strict";// this treats all JS codes as newer versions
 //alert(3+3);//we are using nodeJS not browser no alert in nodeJS
 console.log(3
-    +3)//code readabilty should be high just checking working or not
+    +
+    3)//code readabilty should be high just checking working or not
     
     let nama="Som"
     let age=21
@@ -19,6 +20,8 @@ console.log(3
     //object=>Itminaan mai sikhenge baad mai Zanab
     console.log(typeof "Som");
     console.log(typeof 93);
+    console.table([nama,age,isLoggedIn,state,upperDoab,typeof "Som",typeof 93]);
+    
     
     
 
