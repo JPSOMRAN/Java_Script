de1ec114c1f93882f260898a75812faf55318400
JPSOMRAN <141428557+JPSOMRAN@users.noreply.github.com>
2026-08-19 11:47:39 +0000

    container config done

--- 01_basics/04_StringToN.js
+++ 01_basics/04_StringToN.js
@@ -2,3 +2,9 @@ let str1="Lord"
 let str2="Jagan"
 let str3=str1+str2;
 console.log(str3)
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
