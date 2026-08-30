3096453f849d51c84222208395d3fbe2809046b7
JPSOMRAN <141428557+JPSOMRAN@users.noreply.github.com>
2026-08-30 05:14:51 +0000

    container config done

--- 01_basics/LearnCodeJS/q2.js
+++ 01_basics/LearnCodeJS/q2.js
@@ -7,3 +7,8 @@ setTimeout(()=>{
 },5000)
 
 console.log("This is the 3rd text msg but it'll print after 1st one")
+/*
+1st text msg
+This is the 3rd text msg but it'll print after 1st one
+This is 2nd text msg but print after 5 secs
+*/
