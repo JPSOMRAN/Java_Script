b8fba36198145b48ad475780c67dc33a0797d67e
JPSOMRAN <141428557+JPSOMRAN@users.noreply.github.com>
2026-09-09 02:36:52 +0000

    container config doen

--- 01_basics/LearnCodeJS/operator1.js
+++ 01_basics/LearnCodeJS/operator1.js
@@ -7,10 +7,10 @@ let value;
 console.log("Age:",age,",","Name:",name,",","TrueOrFalse:",isTrue,",","Value:",value);
 
 //OPerators
-let a=30,b=15;
-console.log(a+b);
-console.log(a/b);
-console.log(a-b);
-console.log(a*b);
-console.log(a%b);
-console.log(a**b);
+let a=6,b=2;
+console.log(a+b);//8
+console.log(a/b);//3
+console.log(a-b);//4
+console.log(a*b);//12
+console.log(a%b);//0
+console.log(a**b);//36
---
+++ 01_basics/LearnCodeJS/operator2ass.js
@@ -0,0 +1,16 @@
+//Assignment Operator
+// let c=10;
+// c+=5;//15
+// c-=3;//12
+// c*=2;//24
+// c/=3;//8
+// console.log(c);
+
+//Relational Operators
+let d=10,e=20;
+console.log(d==e);//false
+console.log(d!=e);//true
+console.log(d<e);//true
+console.log(d>e);//false
+console.log(d>=10);//true
+console.log(e<=20);//true
