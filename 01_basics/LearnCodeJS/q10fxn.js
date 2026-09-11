8bbba40accb8b22157dc05dc3d40981409bb8dac
JPSOMRAN <141428557+JPSOMRAN@users.noreply.github.com>
2026-09-11 01:58:46 +0000

    container config done

---
+++ 01_basics/LearnCodeJS/q10fxn.js
@@ -0,0 +1,51 @@
+//DEFAULT FXN CREATION
+// function fun(){//declaring the fxn
+//     console.log("welcome to the dairy")
+// }
+// fun()//calling the fxn
+
+//PASSING PARAMETERS
+function fun(name){
+    console.log("Welcome to the Jungle "+name)
+}
+fun("Jaggu")
+
+//ANOTHER METHOD PARAMETERIZED FXN
+function fun(designation){
+    return "He is an "+designation
+}
+const value=fun("IAS")
+console.log(value)
+
+//ANother eg
+function func(status){
+    return "The Order has been " +status;
+}
+const prod=func("Cancelled")
+console.log(prod)
+
+
+function fxn(messege){
+    return "He is now an "+messege
+}
+const msg=fxn("Officer")
+console.log(msg)
+
+function funcn(operation){
+    return "India launched an "+operation
+}
+const optn=funcn("AirStrike")
+console.log(optn)
+
+
+function fn(chaos){
+    return "AI is aiming to "+chaos
+}
+const extinction=fn("Destroy Humanity")
+console.log(extinction)
+
+function fxxn(devotion){
+    return "MAy the gods watch over"+devotion
+}
+const devout=fxxn("You")
+console.log(devout)
---
+++ 01_basics/LearnCodeJS/q11ArwFxn.js
@@ -0,0 +1,6 @@
+//arrow fxn
+const result=(name)=>"Hello"+name
+console.log(result("Purandhar"))
+
+const res=(nam)=>"Hello"+nam
+console.log(res(Prrajany))
