606538d6e8903045dfb3aa2817edb74961981ed0
JPSOMRAN <141428557+JPSOMRAN@users.noreply.github.com>
2026-09-18 16:30:57 +0000

    container config done

--- 01_basics/LearnCodeJS/q13callback.js
+++ 01_basics/LearnCodeJS/q13callback.js
@@ -2,4 +2,16 @@ function user(name,callback){
     console.log(name)
     callback()
 }
-user ("Akki",()=>console.log("Its done bro"))
+user ("Sonam",()=>console.log("Its done bro!!!"))
+
+function userReply(nam, callback) {
+    console.log(nam);
+
+    setTimeout(() => {
+        callback();
+    }, 3000);
+}
+
+userReply("Sallu", () => {
+    console.log("Its over bro");
+});
